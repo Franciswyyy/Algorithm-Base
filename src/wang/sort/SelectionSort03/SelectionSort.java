54136fcf22fcbd7e3f180e792d58a807ccbe4bbf
wangyu <francis_wangyu@163.com>
2018-06-01 23:27:31 +0800

    测试算法的辅助方法

---
+++ src/wang/sort/SelectionSort03/SelectionSort.java
@@ -0,0 +1,42 @@
+package wang.sort.SelectionSort03;
+
+
+import wang.sort.SelectionSort02.Student;
+
+// 用于测试算法的辅助函数
+public class SelectionSort {
+
+    // 目的不允许产生实例
+    private SelectionSort() {
+    }
+
+    public static void selectSort(Comparable[] arr){
+
+        int len = arr.length;
+        for(int i = 0; i < len; i ++){
+            int minIndex = i;
+            for(int j = i + 1; j < len; j ++){
+                if(arr[j].compareTo(arr[minIndex]) < 0){
+                    minIndex = j;
+                }
+            }
+            swap(arr, i, minIndex);
+        }
+    }
+
+    private static void swap(Object[] arr, int i, int j){
+        Object tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+
+    public static void main(String[] args) {
+
+        int N = 20000;
+        Integer[] arr = SortTestHelper.generateRandomArray(N, 0, 100000);
+        SelectionSort.selectSort(arr);
+        SortTestHelper.printArray(arr);
+        return;
+    }
+
+}
---
+++ src/wang/sortTestHelp/SortTestHelper.java
@@ -0,0 +1,33 @@
+package wang.sortTestHelp;
+
+import java.util.Random;
+
+// 一般用于测试用例， 比如 打印数组，生成随机数组，
+public class SortTestHelper {
+
+    // SortTestHelper不允许产生任何实例
+    private SortTestHelper(){}
+
+    //生成有n个元素的随机数组，每个元素的随机范围为[rangL, rangR]
+    public static Integer[] generateRandomArray(int n, int rangL, int rangR){
+
+        assert rangL <= rangR;
+
+        Integer[] arr = new Integer[n];
+        for(int i = 0; i < n; i ++){
+            arr[i] = new Integer((int)(Math.random() * (rangR- rangL + 1) + rangL));
+        }
+        return arr;
+    }
+
+   //打印arr数组的所有内容
+    public static void printArray(Object arr[]){
+        for (int i = 0; i < arr.length; i++){
+            System.out.print( arr[i] );
+            System.out.print( ' ' );
+        }
+        System.out.println();
+
+        return;
+    }
+}
