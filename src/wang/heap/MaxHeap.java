d95977ad974525bff2295c768ad2a210f0540a43
wangyu <francis_wangyu@163.com>
2018-06-05 09:57:38 +0800

    shift并打印可视化堆

--- src/wang/heap/MaxHeap.java
+++ src/wang/heap/MaxHeap.java
@@ -1,15 +1,18 @@
 package wang.heap;
 
-// 父节点从1开始的，0号索引不用的~
-public class MaxHeap<Item> {
+// 最大堆,父节点从1开始的
+public class MaxHeap<Item extends Comparable>{
+//Item要比较大小,所以要继承Comparable接口
 
-    private Item[] date;
-    private int count;
+    protected Item[] data;
+    protected int count;
+    protected int capacity;
 
     //构造函数，构造一个空堆，可容纳capacity个元素
     public MaxHeap(int capacity) {
-        date = (Item[])new Object[capacity + 1];   // 0索引不用
+        data = (Item[])new Comparable[capacity + 1];   // 0索引不用
         count = 0;
+        this.capacity = capacity;
     }
 
     //返回堆中个数
@@ -22,10 +25,39 @@ public class MaxHeap<Item> {
         return count == 0;
     }
 
+
+    private void shiftUp(int k){
+
+        while(k > 1 && data[k/2].compareTo(data[k]) < 0){
+            swap(k, k/2);
+            k /= 2;
+        }
+    }
+
+    //在最大堆中插入一个新的元素 item
+    public void insert(Item item){
+
+        assert count + 1 <= capacity;
+        data[count + 1] = item;
+        count ++;
+        shiftUp(count);
+    }
+
+    private void swap(int i, int j){
+        Item t = data[i];
+        data[i] = data[j];
+        data[j] = t;
+    }
+
     //test
     public static void main(String[] args) {
 
         MaxHeap<Integer> maxHeap = new MaxHeap<Integer>(100);
+        int N = 10;  //堆中个数
+        int M = 100;  //值的范围
+        for(int i = 0; i < N; i ++){
+            maxHeap.insert(new Integer((int)(Math.random() * M)));
+        }
         System.out.println(maxHeap.size());
     }
 }
