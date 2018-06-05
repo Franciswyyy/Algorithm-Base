7466003dc0ea897ab9aeecca53f8d54e6ed743e0
wangyu <francis_wangyu@163.com>
2018-06-05 15:55:59 +0800

    优化后的索引

--- src/wang/heap/IndexMaxHeap/IndexMaxHeap02.java
+++ src/wang/heap/IndexMaxHeap/IndexMaxHeap02.java
@@ -2,19 +2,25 @@ package wang.heap.IndexMaxHeap;
 
 import java.util.Arrays;
 
-public class IndexMaxHeap<Item extends Comparable> {
+// 最大索引堆
+public class IndexMaxHeap02<Item extends Comparable> {
 
-    protected Item[] data;
-    protected int[] indexes;
+    protected Item[] data;      // 最大索引堆中的数据
+    protected int[] indexes;    // 最大索引堆中的索引, indexes[x] = i 表示索引i在x的位置
+    protected int[] reverse;    // 最大索引堆中的反向索引, reverse[i] = x 表示索引i在x的位置
     protected int count;
     protected int capacity;
 
-    //构造函数，构造一个空堆
-    public IndexMaxHeap(int capacity) {
-        data = (Item[])new Comparable[capacity + 1];
-        this.capacity = capacity;
+    // 构造函数, 构造一个空堆, 可容纳capacity个元素
+    public IndexMaxHeap02(int capacity){
+        data = (Item[])new Comparable[capacity+1];
+        indexes = new int[capacity+1];
+        reverse = new int[capacity+1];
+        for( int i = 0 ; i <= capacity ; i ++ )
+            reverse[i] = 0;
+
         count = 0;
-        indexes = new int[capacity + 1];
+        this.capacity = capacity;
     }
 
     // 返回索引堆中的元素个数
@@ -34,9 +40,13 @@ public class IndexMaxHeap<Item extends Comparable> {
         assert count + 1 <= capacity;
         assert i + 1 >= 1 && i + 1 <= capacity;
 
+        // 再插入一个新元素前,还需要保证索引i所在的位置是没有元素的。
+        assert !contain(i);
+
         i += 1;
         data[i] = item;
         indexes[count+1] = i;
+        reverse[i] = count + 1;
         count ++;
 
         shiftUp(count);
@@ -48,6 +58,7 @@ public class IndexMaxHeap<Item extends Comparable> {
 
         Item ret = data[indexes[1]];
         swapIndexes( 1 , count );
+        reverse[indexes[count]] = 0;
         count --;
         shiftDown(1);
 
@@ -60,6 +71,7 @@ public class IndexMaxHeap<Item extends Comparable> {
 
         int ret = indexes[1] - 1;
         swapIndexes( 1 , count );
+        reverse[indexes[count]] = 0;
         count --;
         shiftDown(1);
 
@@ -78,33 +90,51 @@ public class IndexMaxHeap<Item extends Comparable> {
         return indexes[1]-1;
     }
 
+    // 看索引i所在的位置是否存在元素
+    boolean contain( int i ){
+        assert  i + 1 >= 1 && i + 1 <= capacity;
+        return reverse[i+1] != 0;
+    }
+
     // 获取最大索引堆中索引为i的元素
     public Item getItem( int i ){
-        assert i + 1 >= 1 && i + 1 <= capacity;
+        assert contain(i);
         return data[i+1];
     }
 
     // 将最大索引堆中索引为i的元素修改为newItem
     public void change( int i , Item newItem ){
 
+        assert contain(i);
+
         i += 1;
         data[i] = newItem;
 
         // 找到indexes[j] = i, j表示data[i]在堆中的位置
         // 之后shiftUp(j), 再shiftDown(j)
-        for( int j = 1 ; j <= count ; j ++ )
-            if( indexes[j] == i ){
-                shiftUp(j);
-                shiftDown(j);
-                return;
-            }
+//        for( int j = 1 ; j <= count ; j ++ )
+//            if( indexes[j] == i ){
+//                shiftUp(j);
+//                shiftDown(j);
+//                return;
+//            }
+
+        // 有了 reverse 之后,
+        // 我们可以非常简单的通过reverse直接定位索引i在indexes中的位置
+        shiftUp( reverse[i] );
+        shiftDown( reverse[i] );
     }
 
     // 交换索引堆中的索引i和j
+    // 由于有了反向索引reverse数组，
+    // indexes数组发生改变以后， 相应的就需要维护reverse数组
     private void swapIndexes(int i, int j){
         int t = indexes[i];
         indexes[i] = indexes[j];
         indexes[j] = t;
+
+        reverse[indexes[i]] = i;
+        reverse[indexes[j]] = j;
     }
 
     //********************
@@ -136,22 +166,29 @@ public class IndexMaxHeap<Item extends Comparable> {
         }
     }
 
-    // 测试索引堆中的索引数组index
+    // 测试索引堆中的索引数组index和反向数组reverse
     // 注意:这个测试在向堆中插入元素以后, 不进行extract操作有效
     public boolean testIndexes(){
 
         int[] copyIndexes = new int[count+1];
+        int[] copyReverseIndexes = new int[count+1];
 
-        for( int i = 0 ; i <= count ; i ++ )
+        for( int i = 0 ; i <= count ; i ++ ) {
             copyIndexes[i] = indexes[i];
+            copyReverseIndexes[i] = reverse[i];
+        }
 
         copyIndexes[0] = 0;
+        copyReverseIndexes[0] = 0;
         Arrays.sort(copyIndexes);
+        Arrays.sort(copyReverseIndexes);
 
-        // 在对索引堆中的索引进行排序后, 应该正好是1...count这count个索引
+        // 在对索引堆中的索引和反向索引进行排序后,
+        // 两个数组都应该正好是1...count这count个索引
         boolean res = true;
         for( int i = 1 ; i <= count ; i ++ )
-            if( copyIndexes[i-1] + 1 != copyIndexes[i] ){
+            if( copyIndexes[i-1] + 1 != copyIndexes[i] ||
+                    copyReverseIndexes[i-1] + 1 != copyReverseIndexes[i] ){
                 res = false;
                 break;
             }
