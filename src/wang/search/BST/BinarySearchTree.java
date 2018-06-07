ab1c1e58acf17383c5700f6d4ed31a2a959000ab
wangyu <francis_wangyu@163.com>
2018-06-07 19:06:53 +0800

    bst查询

--- src/wang/search/BST/BinarySearchTree.java
+++ src/wang/search/BST/BinarySearchTree.java
@@ -38,7 +38,6 @@ public class BinarySearchTree <Key extends Comparable<Key>, Value>{
     public void insert(Key key, Value value){
         root = insert(root, key, value);
     }
-
     private Node insert(Node node, Key key, Value value){
 
         if(node == null){
@@ -57,5 +56,42 @@ public class BinarySearchTree <Key extends Comparable<Key>, Value>{
         return node;
     }
 
+
+
+    //查看二叉树中包含key
+    public boolean contain(Key key){
+        return contain(root, key);
+    }
+    private boolean contain(Node node, Key key){
+        if(node == null){
+            return false;
+        }
+
+        if(key.compareTo(node.key) == 0){
+            return true;
+        }else if(key.compareTo(node.key) < 0){
+            return contain(node.left, key);
+        }else {
+            return contain(node.right, key);
+        }
+    }
+
+    //二叉树中搜索键key对应的值，如果key不存在,则返回null
+    public Value search(Key key){
+        return search(root, key);
+    }
+    private Value search(Node node, Key key){
+        //以node为根，,搜索key所对应的value
+        if(node == null){
+            return null;
+        }
+        if(key.compareTo(node.key) == 0){
+            return node.value;
+        }else if(key.compareTo(node.key) > 0){
+            return search(node.right, key);
+        }else {
+            return search(node.left, key);
+        }
+    }
 
 }
