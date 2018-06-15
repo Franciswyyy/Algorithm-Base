3ca4865244e2afbc42029ae5a411141faf01a1ad
wangyu <francis_wangyu@163.com>
2018-06-15 16:23:03 +0800

    添加图的迭代器

--- src/wang/Graph/DenseGraph.java
+++ src/wang/Graph/DenseGraph.java
@@ -1,6 +1,8 @@
 package wang.Graph;
 
 
+import java.util.Vector;
+
 // 稠密图 - 邻接矩阵
 public class DenseGraph {
 
@@ -44,5 +46,17 @@ public class DenseGraph {
         assert v >= 0 && v < n ;
         assert w >= 0 && w < n ;
         return g[v][w];
+    }
+
+    //返回图中一个顶点的所有邻边
+    public Iterable<Integer> adj(int v){
+        assert v >= 0 && v < n;
+        Vector<Integer> adjV = new Vector<Integer>();
+        for(int i = 0; i < n; i ++){
+            if(g[v][i]){
+                adjV.add(i);
+            }
+        }
+        return adjV;
     }
 }
