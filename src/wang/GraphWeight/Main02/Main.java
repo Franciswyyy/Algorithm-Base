91f3fc79aeebd26ac7ad2901780d1bb7ad2a0783
wangyu <francis_wangyu@163.com>
2018-06-16 15:35:05 +0800

    lazy prim

---
+++ src/wang/GraphWeight/Main02/Main.java
@@ -0,0 +1,30 @@
+package wang.GraphWeight.Main02;
+
+import wang.GraphWeight.Edge;
+import wang.GraphWeight.LazyPrimMST;
+import wang.GraphWeight.ReadWeightedGraph;
+import wang.GraphWeight.SparseWeightedGraph;
+
+import java.util.Vector;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String filename = "F:\\Algorithm-Base\\src\\wang\\GraphWeight\\testG1.txt";
+        int V = 8;
+
+        SparseWeightedGraph<Double> g = new SparseWeightedGraph<Double>(V, false);
+        ReadWeightedGraph readGraph = new ReadWeightedGraph(g, filename);
+
+        // Test Lazy Prim MST
+        System.out.println("Test Lazy Prim MST:");
+        LazyPrimMST<Double> lazyPrimMST = new LazyPrimMST<Double>(g);
+        Vector<Edge<Double>> mst = lazyPrimMST.mstEdges();
+        for( int i = 0 ; i < mst.size() ; i ++ )
+            System.out.println(mst.elementAt(i));
+        System.out.println("The MST weight is: " + lazyPrimMST.result());
+
+        System.out.println();
+    }
+}
