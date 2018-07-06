378ad7865f796c81d6f418d977f07c2cebbc530c
wangyu <francis_wangyu@163.com>
2018-07-06 22:02:53 +0800

    tu

---
+++ src/LeetCode/Solution127.java
@@ -0,0 +1,21 @@
+package LeetCode;
+
+import java.util.List;
+
+public class Solution127 {
+
+    /**
+     * 规定了所有的单词都相同的长度，只有小写，没有重复，不为空
+     * @param beginWord
+     * @param endWord
+     * @param wordList
+     * @return
+     */
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+
+        if(!wordList.contains(endWord)) return 0;
+
+
+
+    }
+}
--- src/LeetCode/Solution279.java
+++ src/LeetCode/Solution279.java
@@ -1,4 +1,38 @@
 package LeetCode;
 
+
+
+import javafx.util.Pair;
+
+import java.util.LinkedList;
+
 public class Solution279 {
+
+    // Pair类，配对，类似属于Key,Value的整数对。
+    public int numSquares(int n) {
+
+        LinkedList<Pair<Integer, Integer>> queue = new LinkedList<Pair<Integer, Integer>>();
+        queue.addLast(new Pair<Integer, Integer>(n, 0));
+
+        boolean[] visited = new boolean[n+1];
+        visited[n] = true;
+
+        while(!queue.isEmpty()){
+            Pair<Integer, Integer> front = queue.removeFirst();
+            int num = front.getKey();
+            int step = front.getValue();
+
+            if(num == 0) return step;
+
+            for(int i = 1; num - i*i >= 0; i ++){
+                int a = num - i*i;
+                if(!visited[a]){
+                    if(a == 0) return step + 1;
+                    queue.addLast(new Pair<Integer, Integer>(a, step + 1));
+                    visited[a] = true;
+                }
+            }
+        }
+        throw  new IllegalStateException("No Solution");
+    }
 }
