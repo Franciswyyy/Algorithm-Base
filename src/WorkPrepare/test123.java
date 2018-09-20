c95f9b37f574ce805a1a4d0e11d4dbab4e3c588d
Franciswyyy <826531768@qq.com>
2018-09-20 23:24:44 +0800

    o

---
+++ src/WorkPrepare/test123.java
@@ -0,0 +1,16 @@
+package WorkPrepare;
+
+import java.util.Scanner;
+
+public class test123 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+    }
+
+
+    private static void helper(){
+
+    }
+}
